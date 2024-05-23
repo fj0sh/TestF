f4142aab3c5b15d1f3de4b31873606d281d13d38
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-05-23 16:01:09 +0800

    login

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,28 +1,40 @@
 const authModel = require('../model/auth.model')
+const session = require('express-session')
 
 exports.registerUser = (req, res) => {
     authModel.insertUser(req.body, (err, result) => {
         if (err) {
             console.log(err);
-        }
+        }   
             console.log(result);
             res.send(result);
     })
 }
 
 exports.login = (req, res) => {
-    authModel.loginUser(req.body, (err, result) => {
+    const reqbody = req.body;
+    if (!reqbody) {
+        return res.status(400).send("NO CREDENTIALS");
+    }
+
+    authModel.loginUser(reqbody, (err, result) => {  
         if (err) {
             console.log(err);
+            return res.status(500).send("Internal Server Error");
         }
-        if(result.length == 0){
-            res.status(400).send('No Result');
-            return;
+
+        if (reqbody.username && reqbody.password) {
+            const user = result.find(u => u.username === reqbody.username);
+
+            if (!user) {
+                return res.status(404).send("Account not Found");
+            }
+            return res.status(200).send("Logged In! Token: ");
         }
-            console.log(result);
-            res.send(result);
-    })
-}
+        
+        return res.status(400).send("Invalid Request");
+    });
+};
 
 
 
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyparser = require('body-parser')
 const app = express();
+const session = require('express-session')
 PORT = process.env.PORT;
 
 const authRoute = require("./routes/auth.route");
@@ -10,12 +11,17 @@ const authRoute = require("./routes/auth.route");
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(cors());
+app.use(session({
+    secret: 'some secret',
+    cookie: {maxAge: 30000},
+    saveUninitialized: false
+}))
 
-app.use('/auth',authRoute)
+app.use('/auth', authRoute)
 
 app.listen(PORT, () => {
     console.log('listening to ' + PORT);
-}) 
+})
 
 
 // var currentdate = new Date().toLocaleDateString();
